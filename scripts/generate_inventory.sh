65b4bf6d0c4bd9e7b45e9aa3e45f35cda224f841
adrianmiramar <a.miramar@gmail.com>
2018-11-23 11:24:18 -0600

    Initial Playbook

--- scripts/generate_inventory.sh
+++ scripts/generate_inventory.sh
@@ -1,8 +1,21 @@
 #!/usr/bin/env bash
 
-echo "Copy a new hosts file to /etc/ansible/hosts"
-cp /root/ocp_advanced_deployment_homework/hosts /etc/ansible/hosts
+echo "Creating new folder to /etc/ansible/inventory"
+mkdir /etc/ansible/inventory
+echo "Copy a new hosts file and variables file to /etc/ansible/inventory"
+cp /root/ocp_advanced_deployment_homework/hosts/* /etc/ansible/inventory/
 
-echo "Set the current GUID to generate the inventory"
+echo "Override envoroment vairables to generate the inventory"
 GUID=`hostname|awk -F. '{print $2}'`
-sed -i "s/GUID/$GUID/g" /etc/ansible/hosts
+
+sed -i "s/GUID/$GUID/g" /etc/ansible/inventory/hosts
+sed -i "s/INTERNAL/$INTERNAL/g" /etc/ansible/inventory/hosts
+
+sed -i "s/GUID/$GUID/g" /etc/ansible/inventory/variables
+sed -i "s/DOMAIN/$DOMAIN/g" /etc/ansible/inventory/variables
+sed -i "s/INTERNAL/$INTERNAL/g" /etc/ansible/inventory/variables
+sed -i "s/AUTH_USER/$AUTH_USER/g" /etc/ansible/inventory/variables
+sed -i "s/AUTH_PASSWORD/$AUTH_PASSWORD/g" /etc/ansible/inventory/variables
+
+echo "Modify /etc/ansible/ansible.cfg to use inventory dir as default"
+sed -i "s|#inventory      = /etc/ansible/hosts|inventory       = /etc/ansible/inventory|g" /etc/ansible/ansible.cfg
